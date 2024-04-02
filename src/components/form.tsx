c20311a9615dcd4223383df3a150886810481387
DaniM0613 <wwh.rj.92@gmail.com>
2024-04-01 22:13:02 -0600

    enviando los ultimos cambios

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,9 +1,13 @@
-
+ 
+ 
+// Creando el formulario para calcular las calorias. 
 
 export default function form() {
   return (
-    <div>
-      form
-    </div>
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+       <div className="grid grid-cols-1 gap-3">
+         <label htmlFor="">Categoria: </label>
+       </div>
+    </form>
   )
 }
